283af525a4e07f47e2fd9adc687bc387229a3e7b
Bai Jaitrong <bjaitrong@comcast.net>
2023-01-31 15:23:27 -0700

    Add LoginViewModel, Index action, Login view, and Post Register action.

--- PierresSweetAndSavoryTreats/Controllers/AccountControllers.cs
+++ PierresSweetAndSavoryTreats/Controllers/AccountControllers.cs
@@ -29,8 +29,37 @@ namespace PierresSweetAndSavoryTreats.Controllers
       return View();
     }
 
-    
-  
-  }
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      if(!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        ApplicationUser user = new ApplicationUser { UserName = model.Email };
+        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
 
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          foreach ( IdentityError error in result.Errors)
+          {
+            ModelState.AddModelError("", error.Description);
+          }
+
+          return View(model);
+        }
+      }
+    }
+
+    public ActionResult Login()
+    {
+      return View();
+    }
+  }
 }
