0a1cb6cf20cfc2c317eb7ae74fe658308dbb7f8a
Bai Jaitrong <bjaitrong@comcast.net>
2023-02-01 03:11:14 -0700

    Add AddTreat and DeleteJoin action, and AddTreat view.

--- PierresSweetAndSavoryTreats/Controllers/FlavorsController.cs
+++ PierresSweetAndSavoryTreats/Controllers/FlavorsController.cs
@@ -113,5 +113,47 @@ namespace PierresSweetAndSavoryTreats.Controllers
 
       return RedirectToAction("Index");
     }
+
+    [Authorize]
+    public ActionResult AddTreat(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
+
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddTreat(Flavor flavor, int treatId)
+    {
+      string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+
+      #nullable enable
+      FlavorTreat? joinEntity = _db.FlavorTreats.FirstOrDefault(join => (join.TreatId == treatId && join.FlavorId == flavor.FlavorId));
+      #nullable disable
+
+      if (joinEntity == null && treatId != 0)
+      {
+        _db.FlavorTreats.Add(new FlavorTreat() {TreatId = treatId, FlavorId = flavor.FlavorId});
+        _db.SaveChanges();
+      }
+
+      return RedirectToAction("Details", new { id = flavor.FlavorId});
+    } 
+
+    [Authorize]
+    [HttpPost]
+    public async Task <ActionResult> DeleteJoin(int joinId)
+    {
+      string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+
+      FlavorTreat joinEntry = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreats.Remove(joinEntry);
+      _db.SaveChanges();
+
+      return RedirectToAction("Index");
+    }
   }
 }
